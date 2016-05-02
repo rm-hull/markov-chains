c3e399771f0d9473b747e598130a78148e9fc550
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-02 19:18:19 +0100

    Overtone demo

--- test/examples/algorithmic_music.clj
+++ test/examples/algorithmic_music.clj
@@ -1,9 +1,33 @@
 (ns examples.algorithmic-music
   (:require
-    [markov-chains.core :refer :all]))
+    [markov-chains.core :refer [generate]]
+    [overtone.core :refer :all]))
 
 (def first-order-prob-matrix {
-  "A" { "A" 0.1 "C♯" 0.6 "E♭" 0.3 }
-  "C♯" { "A" 0.25 "C♯" 0.05 "E♭" 0.7 }
-  "E♭" { "A" 0.7 "C♯" 0.3 "E♭" 0 }})
+  :A4  { :A4 0.1  :C#4 0.6  :Eb5 0.3 }
+  :C#4 { :A4 0.25 :C#4 0.05 :Eb5 0.7 }
+  :Eb5 { :A4 0.7  :C#4 0.3  :Eb5 0 }})
 
+(take 30 (generate first-order-prob-matrix))
+
+(comment
+
+  (boot-internal-server)
+
+  (definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
+    (* (env-gen (env-lin attack sustain release) 1 1 0 1 FREE)
+       (saw freq)
+       vol))
+
+  (defn saw2 [music-note]
+      (saw-wave (midi->hz (note music-note))))
+
+  (defn play-chord [a-chord]
+    (doseq [note a-chord] (saw2 note)))
+
+  (defn chord-progression-time [notes]
+    (doseq [[time note] (map list (iterate (partial + 400) (now)) notes)]
+      (at time (play-chord (chord note :major)))))
+
+  (chord-progression-time (take 30 (generate first-order-prob-matrix)))
+)
